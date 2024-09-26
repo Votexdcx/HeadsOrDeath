aa0184b315472abc4fb34af9771567b6d0c96efa
portuga4k <francisco.abreu195@gmail.com>
2024-09-26 11:41:30 +0100

    merge changes
    merge com a branch do jon

---
+++ HeadsOrDeath/Source/HeadsOrDeath/Public/Characters/Jon.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Jon.generated.h"
+
+UCLASS()
+class HEADSORDEATH_API AJon : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AJon();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void Movefoward(float Value);
+	void Moveside(float Value);
+	void Lookup(float Value);
+	void Lookaround(float Value);
+
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent *Camera;
+	
+	UPROPERTY(EditAnywhere)
+	class USpringArmComponent *SpringArm;
+
+	UPROPERTY(EditAnywhere)
+	class USkeletalMeshComponent *SkeletalMesh;
+};
