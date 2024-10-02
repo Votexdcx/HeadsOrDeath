632f6c47491668687a0de9e1930d423a67f592af
k3vinszn <kevinriscado98@gmail.com>
2024-10-02 20:19:20 +0100

    folder fix2

--- HeadsOrDeath/Intermediate/Build/Mac/arm64/HeadsOrDeathEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "/Users/joaopereira/iCloud Drive (Archive)/Desktop/GitHub/HeadsOrDeath/HeadsOrDeath/Intermediate/Build/Mac/arm64/HeadsOrDeathEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- HeadsOrDeath/Intermediate/Build/Mac/arm64/HeadsOrDeathEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "/Users/joaopereira/iCloud Drive (Archive)/Desktop/GitHub/HeadsOrDeath/HeadsOrDeath/Intermediate/Build/Mac/arm64/HeadsOrDeathEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
